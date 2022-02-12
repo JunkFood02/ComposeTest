edda8e97762ea3dcd763d08f6a906027cb336f2f
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-02-13 00:00:03 +0800

    Implement Coil for image loading

--- app/src/main/java/com/hustunique/composetest/MainActivity.kt
+++ app/src/main/java/com/hustunique/composetest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.hustunique.composetest
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -8,20 +10,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.hustunique.composetest.ui.theme.ComposeTestTheme
 import com.hustunique.composetest.ui.theme.DateTextStyle
 import com.hustunique.composetest.ui.theme.TitleTextStyle
+import com.skydoves.landscapist.glide.GlideImage
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -35,10 +42,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LazyColumn {
-                        items(5) { index ->
-                            val title = "菜狗 $index"
-                            GreetingCard(getDate(), title) { makeToast(title) }
+                    val musicDiaryList = remember { mutableStateListOf<MusicDairyItem>() }
+                    musicDiaryList.add(MusicDairyItem("菜狗 1", getDate(), R.drawable.bg_silent))
+                    musicDiaryList.add(MusicDairyItem("菜狗 2", getDate(), R.drawable.bg_groove))
+                    musicDiaryList.add(MusicDairyItem("菜狗 3", getDate(), R.drawable.bg_hopeful))
+                    musicDiaryList.add(MusicDairyItem("菜狗 4", getDate(), R.drawable.bg_passion))
+                    musicDiaryList.add(MusicDairyItem("菜狗 5", getDate(), R.drawable.bg_romance))
+                    Box() {
+                        LazyColumn() {
+                            itemsIndexed(musicDiaryList) { _, item ->
+                                GreetingCard(
+                                    date = item.date,
+                                    title = item.title,
+                                    resID = item.resId
+                                ) {
+                                    makeToast(item.title)
+                                }
+                            }
+                        }
+                        FloatingActionButton(
+                            modifier = Modifier.size(48.dp),
+                            onClick = {
+                                musicDiaryList.add(
+                                    MusicDairyItem(
+                                        "新菜狗",
+                                        getDate(),
+                                        R.drawable.bg_passion
+                                    )
+                                )
+                            }) {
+
                         }
                     }
                 }
@@ -46,37 +79,44 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun getDate(): String {
-        val simpleDateFormat = SimpleDateFormat("MM月dd日 E HH:mm", Locale.CHINA)
-        return simpleDateFormat.format(Date())
-    }
 
     private fun makeToast(title: String) {
         Toast.makeText(this, "$title was clicked.", Toast.LENGTH_SHORT).show();
     }
+
+    private fun initResourceLists() {
+
+    }
+
+    companion object {
+        fun getDate(): String {
+            val simpleDateFormat = SimpleDateFormat("MM月dd日 E HH:mm", Locale.CHINA)
+            return simpleDateFormat.format(Date())
+        }
+    }
 }
 
+
 @Composable
-fun GreetingCard(date: String, title: String, onClick: () -> Unit) {
+fun GreetingCard(date: String, title: String, resID: Int, onClick: () -> Unit) {
     Card(
         modifier = Modifier
-            .padding(Dp(12F))
+            .padding(12.dp)
             .fillMaxWidth()
             .wrapContentHeight(),
-        elevation = Dp(12F),
-        shape = RoundedCornerShape(Dp(12F))
+        elevation = 12.dp,
+        shape = RoundedCornerShape(12.dp)
     ) {
         Column(modifier = Modifier.clickable(onClick = onClick)) {
             Image(
-                painter = painterResource(id = R.drawable.bg_silent),
-                contentDescription = "Title Picture",
+                painter = rememberImagePainter(resID), contentDescription = "Title Picture",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(width = Dp.Unspecified, height = Dp(150F))
+                    .size(width = Dp.Unspecified, height = 150.dp)
             )
             Column(
-                modifier = Modifier.padding(Dp(12F), Dp(9F))
+                modifier = Modifier.padding(12.dp, 9.dp)
             ) {
                 Text(
                     text = "# $date",
---
+++ app/src/main/java/com/hustunique/composetest/MusicDairyItem.kt
@@ -0,0 +1,7 @@
+package com.hustunique.composetest
+
+class MusicDairyItem(
+    var title: String,
+    var date: String,
+    var resId: Int
+)
