f8254603203a92fdd986376aa96518441144fe05
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-02-13 01:48:43 +0800

    Added long-click gesture for card

--- app/src/main/java/com/hustunique/composetest/MainActivity.kt
+++ app/src/main/java/com/hustunique/composetest/MainActivity.kt
@@ -6,18 +6,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
@@ -31,6 +37,7 @@ import com.hustunique.composetest.ui.theme.ComposeTestTheme
 import com.hustunique.composetest.ui.theme.DateTextStyle
 import com.hustunique.composetest.ui.theme.TitleTextStyle
 import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -45,35 +52,44 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     val musicDiaryList = remember { mutableStateListOf<MusicDairyItem>() }
+                    val listState = rememberLazyListState();
+                    val coroutineScope = rememberCoroutineScope()
                     musicDiaryList.add(MusicDairyItem("菜狗 1", getDate(), R.drawable.bg_silent))
                     musicDiaryList.add(MusicDairyItem("菜狗 2", getDate(), R.drawable.bg_groove))
                     musicDiaryList.add(MusicDairyItem("菜狗 3", getDate(), R.drawable.bg_hopeful))
                     musicDiaryList.add(MusicDairyItem("菜狗 4", getDate(), R.drawable.bg_passion))
                     musicDiaryList.add(MusicDairyItem("菜狗 5", getDate(), R.drawable.bg_romance))
-                    Box() {
-                        LazyColumn() {
-                            itemsIndexed(musicDiaryList) { _, item ->
-                                GreetingCard(
-                                    date = item.date,
-                                    title = item.title,
-                                    resID = item.resId
-                                ) {
+                    LazyColumn(state = listState) {
+                        itemsIndexed(musicDiaryList) { _, item ->
+                            GreetingCard(
+                                date = item.date,
+                                title = item.title,
+                                resID = item.resId, {
                                     makeToast(item.title)
+                                }, {
+                                    makeToast(item.title + "(long click)")
                                 }
-                            }
+                            )
                         }
-                        FloatingActionButton(
-                            modifier = Modifier.size(48.dp),
-                            onClick = {
-                                musicDiaryList.add(
-                                    MusicDairyItem(
-                                        "新菜狗",
-                                        getDate(),
-                                        R.drawable.bg_passion
-                                    )
-                                )
-                            }) {
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(36.dp),
+                        contentAlignment = Alignment.BottomEnd
+                    ) {
 
+                        FAB() {
+                            musicDiaryList.add(
+                                MusicDairyItem(
+                                    "新菜狗",
+                                    getDate(),
+                                    R.drawable.bg_passion
+                                )
+                            )
+                            coroutineScope.launch {
+                                listState.animateScrollToItem(index = musicDiaryList.size - 1)
+                            }
                         }
                     }
                 }
@@ -86,9 +102,6 @@ class MainActivity : ComponentActivity() {
         Toast.makeText(this, "$title was clicked.", Toast.LENGTH_SHORT).show();
     }
 
-    private fun initResourceLists() {
-
-    }
 
     companion object {
         fun getDate(): String {
@@ -98,13 +111,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview
 @Composable
 fun GreetingCard(
     date: String = getDate(),
     title: String = "菜狗",
     resID: Int = R.drawable.bg_romance,
-    onClick: () -> Unit = {}
+    onClick: () -> Unit = {},
+    onLongClick: () -> Unit = {}
 ) {
     Card(
         modifier = Modifier
@@ -114,7 +129,14 @@ fun GreetingCard(
         elevation = 12.dp,
         shape = RoundedCornerShape(12.dp)
     ) {
-        Column(modifier = Modifier.clickable(onClick = onClick)) {
+        Column(
+            Modifier.combinedClickable(
+                enabled = true,
+                null,
+                onLongClick = onLongClick,
+                onClick = onClick
+            )
+        ) {
             Image(
                 painter = rememberImagePainter(resID), contentDescription = "Title Picture",
                 contentScale = ContentScale.Crop,
@@ -140,7 +162,14 @@ fun GreetingCard(
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FAB(onClick: () -> Unit) {
+    FloatingActionButton(
+        modifier = Modifier
+            .size(64.dp), onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.outline_flutter_dash_24),
+            contentDescription = "add new item"
+        )
+    }
 }
-
